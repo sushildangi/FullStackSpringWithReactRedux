3847b58f5bc85ddb4ff187d30569234f6fc4f3f1
Sushil Dangi <dangi.sushil5@gmail.com>
2018-11-16 23:46:02 +0530

    Project Strating

---
+++ ProjectManagementTool/src/main/java/org/pyarts/projectmanagementtool/ProjectManagementToolApplication.java
@@ -0,0 +1,12 @@
+package org.pyarts.projectmanagementtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManagementToolApplication.class, args);
+    }
+}
