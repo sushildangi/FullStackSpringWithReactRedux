d70b9a11b9066ad77dbd398c90c07fe63c74c581
sushildangi <dangi.sushil5@gmail.com>
2018-11-26 13:21:57 +0530

    Adding Bootstrap 4 in Project

--- project-management-tool/src/App.js
+++ project-management-tool/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from './components/Layout/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+       <Header/>
        <Dashboard/>
       </div>
     );
--- project-management-tool/src/components/Dashboard.js
+++ project-management-tool/src/components/Dashboard.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import ProjectItem from './Project/ProjectItem';
-import Header from './Layout/Header';
+
 
 class Dashboard extends Component {
   render() {
     return (
       <div>
-        <Header/>
-        <h1>Welcome to Dashboard</h1>
+        <h1 className="alert alert-warning">Welcome to Dashboard</h1>
+        <ProjectItem/>
+        <ProjectItem/>
         <ProjectItem/>
        </div>
     )
